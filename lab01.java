32f5be3525ec7e815b58ce6e69d2368b6e7aec4b
leecj27 <leecj27@uww.edu>
2015-02-05 16:34:55 -0600

    initial version

---
+++ lab01.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Lab01
+{
+	public static void main(String[] args)
+	{
+		Scanner reader = new Scanner(System.in);
+		int counter = 3;
+		int randomNumber = 5 + (int)(Math.random()*10); 
+
+		while (counter != 0)
+		{
+			System.out.println("Try to guess the number between 0 
+and 10! Guesses: " + counter);
+			int guess=reader.nextInt();
+			
+			if (guess == randomNumber)
+			{
+				System.out.println("Winner!");
+				counter = 0;
+			}
+			else 
+			{
+				System.out.println("Try Again.\n");
+				counter--;
+			}
+		}
+
+	}
+}
+
